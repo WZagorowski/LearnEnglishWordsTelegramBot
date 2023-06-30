44281c7a5c5809bec0c17d7c4398af04788f6b73
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-30 17:52:50 +0200

    TelegramBot-2 create data class Word and MutableList<Word>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,23 @@
 import java.io.File
 
 fun main() {
-
     val wordsFile = File("words.txt")
-    wordsFile.createNewFile()
+    val dictionary = Dictionary()
 
-    for (line in wordsFile.readLines())
-        println(line)
+    for (line in wordsFile.readLines()) {
+        val line = line.split("|")
+        val word = Word(original = line[0], translate = line[1], correctAnswersCount = line[2].toInt())
+        dictionary.listOfWords.add(word)
+    }
+    dictionary.listOfWords.forEach { println(it) }
 }
+
+class Dictionary {
+    val listOfWords: MutableList<Word> = mutableListOf()
+}
+
+data class Word(
+    val original: String,
+    val translate: String,
+    val correctAnswersCount: Int = 0,
+)
