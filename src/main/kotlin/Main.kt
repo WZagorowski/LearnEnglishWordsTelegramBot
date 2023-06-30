611cc8defba28a24c2ba3f59a1a83c93f20fceb0
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-30 22:10:59 +0200

    TelegramBot-3 menu with statistics

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,32 @@ import java.io.File
 
 fun main() {
     val wordsFile = File("words.txt")
-    val dictionary = Dictionary()
+    val dictionary = Dictionary("English dictionary")
 
     for (line in wordsFile.readLines()) {
         val line = line.split("|")
         val word = Word(original = line[0], translate = line[1], correctAnswersCount = line[2].toInt())
         dictionary.listOfWords.add(word)
     }
-    dictionary.listOfWords.forEach { println(it) }
+
+    while (true) {
+        println("Меню: 1 – Учить слова, 2 – Статистика, 0 – Выход")
+
+        when (readln()) {
+            "1" -> println("1")
+            "2" -> {
+                val numberCorrectAnswers = (dictionary.listOfWords.filter { it.correctAnswersCount > 2 }).size
+                val numberAllAnswers = (dictionary.listOfWords).size
+                val percentCorrectAnswers = 100 * numberCorrectAnswers / numberAllAnswers
+                println("Выучено $numberCorrectAnswers из $numberAllAnswers слов | $percentCorrectAnswers%")
+            }
+            "0" -> return
+            else -> println("Ошибка! Введен неправильный тип данных.")
+        }
+    }
 }
 
-class Dictionary {
+data class Dictionary(val name: String) {
     val listOfWords: MutableList<Word> = mutableListOf()
 }
 
